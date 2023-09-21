dcc3b5270a299504e2626b749fc77c454f559acd
MariuzM <mariusdotdev@gmail.com>
2023-09-21 14:49:57 +0700

    test key and class events

--- main.cpp
+++ main.cpp
@@ -3,28 +3,6 @@
 
 using namespace std;
 
-class Box {
-   public:
-    Box(int x, int y, int width, int height) {
-        this->x = x;
-        this->y = y;
-        this->width = width;
-        this->height = height;
-    }
-    sf::RectangleShape getRect() {
-        sf::RectangleShape box;
-        box.setPosition(sf::Vector2f(this->x, this->y));
-        box.setSize(sf::Vector2f(this->width, this->height));
-        return box;
-    }
-
-   private:
-    int x;
-    int y;
-    int width;
-    int height;
-};
-
 // Function to handle events
 void handleEvents(sf::RenderWindow& window) {
     sf::Event event;
@@ -44,47 +22,86 @@ void handleEvents(sf::RenderWindow& window) {
     }
 }
 
-// Function to update game state
-void updateGameState(sf::RectangleShape& box, sf::Vector2f& boxPos, int& xVelocity, int& yVelocity, int& boxSizeX, int& boxSizeY, sf::RenderWindow& window) {
-    if (boxPos.x < 0 || boxPos.x > window.getSize().x - boxSizeX) {
-        xVelocity *= -1;
+// // Function to update game state
+// void updateGameState(Box& box, int& xVelocity, int& yVelocity, int& boxSizeX, int& boxSizeY, sf::RenderWindow& window) {
+//     if (box.getPosX() < 0 || box.getPosX() > window.getSize().x - boxSizeX) {
+//         xVelocity *= -1;
+//     }
+
+//     if (box.getPosY() < 0 || box.getPosY() > window.getSize().y - boxSizeY) {
+//         yVelocity *= -1;
+//     }
+
+//     box.setPosX(xVelocity);
+//     box.setPosY(yVelocity);
+// }
+
+class Box {
+   private:
+    int x;
+    int y;
+    int w;
+    int h;
+
+   public:
+    explicit Box(int _x, int _y, int _w, int _h) : x(_x), y(_y), w(_w), h(_h) {}
+
+    sf::RectangleShape getRect() {
+        sf::RectangleShape rect;
+        rect.setPosition(sf::Vector2f(x, y));
+        rect.setSize(sf::Vector2f(w, h));
+        return rect;
     }
 
-    if (boxPos.y < 0 || boxPos.y > window.getSize().y - boxSizeY) {
-        yVelocity *= -1;
+    sf::Vector2f getPosition() {
+        return sf::Vector2f(x, y);
     }
 
-    boxPos.x += xVelocity;
-    boxPos.y += yVelocity;
-    box.setPosition(boxPos);
+    void setPosition(int _x, int _y) {
+        x = _x;
+        y = _y;
+    }
+};
+
+void handleKeyboardEvents(Box& box) {
+    int val = 5;
+
+    if (sf::Keyboard::isKeyPressed(sf::Keyboard::Left)) {
+        box.setPosition(box.getPosition().x - val, box.getPosition().y);
+    }
+
+    if (sf::Keyboard::isKeyPressed(sf::Keyboard::Right)) {
+        box.setPosition(box.getPosition().x + val, box.getPosition().y);
+    }
+
+    if (sf::Keyboard::isKeyPressed(sf::Keyboard::Up)) {
+        box.setPosition(box.getPosition().x, box.getPosition().y - val);
+    }
+
+    if (sf::Keyboard::isKeyPressed(sf::Keyboard::Down)) {
+        box.setPosition(box.getPosition().x, box.getPosition().y + val);
+    }
 }
 
 int main() {
-    sf::RenderWindow window(sf::VideoMode(550, 500), "Box");
+    sf::RenderWindow window(sf::VideoMode(700, 500), "Box");
     window.setFramerateLimit(60);
 
-    int boxSizeX = 50;
-    int boxSizeY = 50;
+    Box box(200, 200, 50, 50);
 
-    Box box_1(200, 200, 50, 50);
-
-    sf::RectangleShape box_2;
-    sf::Vector2f boxPos(100, 100);
-
-    box_2.setPosition(boxPos);
-    box_2.setSize(sf::Vector2f(boxSizeX, boxSizeY));
-
-    int xVelocity = 7;
-    int yVelocity = 7;
+    // int xVelocity = 7;
+    // int yVelocity = 7;
+    // int boxSizeX = 50;
+    // int boxSizeY = 50;
 
     while (window.isOpen()) {
         handleEvents(window);
+        handleKeyboardEvents(box);
 
-        updateGameState(box_2, boxPos, xVelocity, yVelocity, boxSizeX, boxSizeY, window);
+        // updateGameState(box, xVelocity, yVelocity, boxSizeX, boxSizeY, window);
 
         window.clear();
-        window.draw(box_1.getRect());
-        window.draw(box_2);
+        window.draw(box.getRect());
         window.display();
     }
 
